265d9da870f2ada487c11572879ca583feeaeff3
AD\mayumi.ohta <mayumi.ohta@zo.uni-heidelberg.de>
2016-09-18 00:31:55 +0200

    add contextwise learning

---
+++ cnn_context.py
@@ -0,0 +1,274 @@
+# -*- coding: utf-8 -*-
+
+##########################################################
+#
+# Attention-based Convolutional Neural Network
+#   for Context-wise Learning
+#
+#
+#   Note: this implementation is mostly based on
+#   https://github.com/yuhaozhang/sentence-convnet/blob/master/model.py
+#
+##########################################################
+
+import tensorflow as tf
+
+# model parameters
+tf.app.flags.DEFINE_integer('batch_size', 100, 'Training batch size')
+tf.app.flags.DEFINE_integer('emb_size', 300, 'Size of word embeddings')
+tf.app.flags.DEFINE_integer('num_kernel', 100, 'Number of filters for each window size')
+tf.app.flags.DEFINE_integer('min_window', 3, 'Minimum size of filter window')
+tf.app.flags.DEFINE_integer('max_window', 5, 'Maximum size of filter window')
+tf.app.flags.DEFINE_integer('vocab_size', 40000, 'Vocabulary size')
+tf.app.flags.DEFINE_integer('num_classes', 10, 'Number of class to consider')
+tf.app.flags.DEFINE_integer('sent_len', 400, 'Input sentence length.')
+tf.app.flags.DEFINE_float('l2_reg', 1e-4, 'l2 regularization weight')
+tf.app.flags.DEFINE_boolean('attention', False, 'Whether use attention or not')
+tf.app.flags.DEFINE_boolean('multi_label', False, 'Multilabel or not')
+
+
+
+
+def _variable_on_cpu(name, shape, initializer):
+    with tf.device('/cpu:0'):
+        var = tf.get_variable(name, shape, initializer=initializer)
+    return var
+
+def _variable_with_weight_decay(name, shape, initializer, wd):
+    var = _variable_on_cpu(name, shape, initializer)
+    if wd is not None and wd != 0.:
+        weight_decay = tf.mul(tf.nn.l2_loss(var), wd, name='weight_loss')
+    else:
+        weight_decay = tf.constant(0.0, dtype=tf.float32)
+    return var, weight_decay
+
+def _auc_pr(true, prob, threshold):
+    pred = tf.select(prob > threshold, tf.ones_like(prob), tf.zeros_like(prob))
+    tp = tf.logical_and(tf.cast(pred, tf.bool), tf.cast(true, tf.bool))
+    fp = tf.logical_and(tf.cast(pred, tf.bool), tf.logical_not(tf.cast(true, tf.bool)))
+    fn = tf.logical_and(tf.logical_not(tf.cast(pred, tf.bool)), tf.cast(true, tf.bool))
+    pre = tf.truediv(tf.reduce_sum(tf.cast(tp, tf.int32)),
+                     tf.reduce_sum(tf.cast(tf.logical_or(tp, fp), tf.int32)))
+    rec = tf.truediv(tf.reduce_sum(tf.cast(tp, tf.int32)),
+                     tf.reduce_sum(tf.cast(tf.logical_or(tp, fn), tf.int32)))
+    return pre, rec
+
+
+class Model(object):
+
+    def __init__(self, config, is_train=True):
+        self.is_train = is_train
+        self.emb_size = config['emb_size']
+        self.batch_size = config['batch_size']
+        self.num_kernel = config['num_kernel']
+        self.min_window = config['min_window']
+        self.max_window = config['max_window']
+        self.vocab_size = config['vocab_size']
+        self.num_classes = config['num_classes']
+        self.sent_len = config['sent_len']
+        self.l2_reg = config['l2_reg']
+        self.multi_instance = config['attention']
+        self.multi_label = config['multi_label']
+        if is_train:
+            self.optimizer = config['optimizer']
+            self.dropout = config['dropout']
+        self.build_graph()
+
+    def conv_layer(self, input, context):
+        pool_tensors = []
+        losses = []
+        for k_size in range(self.min_window, self.max_window+1):
+            with tf.variable_scope('conv-%d-%s' % (k_size, context)) as scope:
+                kernel, wd = _variable_with_weight_decay(
+                    name='kernel-%d-%s' % (k_size, context),
+                    shape=[k_size, self.emb_size, 1, self.num_kernel],
+                    initializer=tf.truncated_normal_initializer(stddev=0.01),
+                    wd=self.l2_reg)
+                losses.append(wd)
+                conv = tf.nn.conv2d(input=input, filter=kernel, strides=[1,1,1,1], padding='VALID')
+                biases = _variable_on_cpu('bias-%d-%s' % (k_size, context),
+                                          [self.num_kernel], tf.constant_initializer(0.0))
+                bias = tf.nn.bias_add(conv, biases)
+                activation = tf.nn.relu(bias, name=scope.name)
+                # shape of activation: [batch_size, conv_len, 1, num_kernel]
+                conv_len = activation.get_shape()[1]
+                pool = tf.nn.max_pool(activation, ksize=[1,conv_len,1,1], strides=[1,1,1,1], padding='VALID')
+                # shape of pool: [batch_size, 1, 1, num_kernel]
+                pool_tensors.append(pool)
+
+        # Combine pooled tensors
+        num_filters = self.max_window - self.min_window + 1
+        pool_size = num_filters * self.num_kernel # 300
+        pool_layer = tf.concat(num_filters, pool_tensors, name='pool-%s' % context)
+        pool_flat = tf.reshape(pool_layer, [-1, pool_size])
+
+        return losses, pool_flat
+
+
+    def build_graph(self):
+        """ Build the computation graph. """
+        self._left = tf.placeholder(dtype=tf.int64, shape=[None, self.sent_len], name='input_left')
+        self._middle = tf.placeholder(dtype=tf.int64, shape=[None, self.sent_len], name='input_middle')
+        self._right = tf.placeholder(dtype=tf.int64, shape=[None, self.sent_len], name='input_right')
+        self._labels = tf.placeholder(dtype=tf.float32, shape=[None, self.num_classes], name='input_y')
+        self._attention = tf.placeholder(dtype=tf.float32, shape=[None, 1], name='attention')
+        losses = []
+
+
+        with tf.variable_scope('embedding-left') as scope:
+            self._W_emb_left = _variable_on_cpu(name=scope.name, shape=[self.vocab_size, self.emb_size],
+                                      initializer=tf.random_uniform_initializer(minval=-1.0, maxval=1.0))
+            sent_batch_left = tf.nn.embedding_lookup(params=self._W_emb_left, ids=self._left)
+            input_left = tf.expand_dims(sent_batch_left, -1)
+
+        with tf.variable_scope('embedding-middle') as scope:
+            self._W_emb_middle = _variable_on_cpu(name=scope.name, shape=[self.vocab_size, self.emb_size],
+                                      initializer=tf.random_uniform_initializer(minval=-1.0, maxval=1.0))
+            sent_batch_middle = tf.nn.embedding_lookup(params=self._W_emb_middle, ids=self._middle)
+            input_middle = tf.expand_dims(sent_batch_middle, -1)
+
+        with tf.variable_scope('embedding-right') as scope:
+            self._W_emb_right = _variable_on_cpu(name=scope.name, shape=[self.vocab_size, self.emb_size],
+                                      initializer=tf.random_uniform_initializer(minval=-1.0, maxval=1.0))
+            sent_batch_right = tf.nn.embedding_lookup(params=self._W_emb_right, ids=self._right)
+            input_right = tf.expand_dims(sent_batch_right, -1)
+
+        # conv + pooling layer
+        contexts = []
+        for contextwise_input, context in zip([input_left, input_middle, input_right],
+                                              ['left', 'middle', 'right']):
+            conv_losses, pool_flat = self.conv_layer(contextwise_input, context)
+            losses.extend(conv_losses)
+            contexts.append(pool_flat)
+        # Combine context tensors
+        num_filters = self.max_window - self.min_window + 1
+        pool_size = num_filters * self.num_kernel # 300
+        concat_context = tf.concat(1, contexts, name='concat')
+        flat_context = tf.reshape(concat_context, [-1, pool_size*3])
+
+
+        # drop out layer
+        if self.is_train and self.dropout > 0:
+            pool_dropout = tf.nn.dropout(flat_context, 1 - self.dropout)
+        else:
+            pool_dropout = flat_context
+
+        # fully-connected layer
+        with tf.variable_scope('output') as scope:
+            W, wd = _variable_with_weight_decay('W', shape=[pool_size*3, self.num_classes],
+                                initializer=tf.truncated_normal_initializer(stddev=0.05), wd=self.l2_reg)
+            losses.append(wd)
+            biases = _variable_on_cpu('bias', shape=[self.num_classes],
+                                      initializer=tf.constant_initializer(0.01))
+            self.logits = tf.nn.bias_add(tf.matmul(pool_dropout, W), biases, name='logits')
+
+        # loss
+        with tf.variable_scope('loss') as scope:
+            if self.multi_label:
+                cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(self.logits, self._labels,
+                                                                        name='cross_entropy_per_example')
+            else:
+                cross_entropy = tf.nn.softmax_cross_entropy_with_logits(self.logits, self._labels,
+                                                                        name='cross_entropy_per_example')
+
+            if self.is_train and self.multi_instance: # apply attention
+                cross_entropy_loss = tf.reduce_sum(tf.mul(cross_entropy, self._attention),
+                                                   name='cross_entropy_loss')
+            else:
+                cross_entropy_loss = tf.reduce_mean(cross_entropy, name='cross_entropy_loss')
+
+            losses.append(cross_entropy_loss)
+            self._total_loss = tf.add_n(losses, name='total_loss')
+
+        # eval with auc-pr metric
+        with tf.variable_scope('evaluation') as scope:
+            precision = []
+            recall = []
+            for threshold in range(10, -1, -1):
+                pre, rec = _auc_pr(self._labels, self.logits, threshold * 0.1)
+                precision.append(pre)
+                recall.append(rec)
+            self._eval_op = zip(precision, recall)
+
+        # train on a batch
+        self._lr = tf.Variable(0.0, trainable=False)
+        if self.is_train:
+            if self.optimizer == 'adadelta':
+                opt = tf.train.AdadeltaOptimizer(self._lr)
+            elif self.optimizer == 'adagrad':
+                opt = tf.train.AdagradOptimizer(self._lr)
+            elif self.optimizer == 'adam':
+                opt = tf.train.AdamOptimizer(self._lr)
+            elif self.optimizer == 'sgd':
+                opt = tf.train.GradientDescentOptimizer(self._lr)
+            else:
+                raise ValueError("Optimizer not supported.")
+            grads = opt.compute_gradients(self._total_loss)
+            self._train_op = opt.apply_gradients(grads)
+
+            for var in tf.trainable_variables():
+                tf.histogram_summary(var.op.name, var)
+        else:
+            self._train_op = tf.no_op()
+
+        return
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def middle(self):
+        return self._middle
+
+    @property
+    def right(self):
+        return self._right
+
+    @property
+    def labels(self):
+        return self._labels
+
+    @property
+    def attention(self):
+        return self._attention
+
+    @property
+    def lr(self):
+        return self._lr
+
+    @property
+    def train_op(self):
+        return self._train_op
+
+    @property
+    def total_loss(self):
+        return self._total_loss
+
+    @property
+    def eval_op(self):
+        return self._eval_op
+
+    @property
+    def scores(self):
+        return self.logits
+
+    @property
+    def W_emb_left(self):
+        return self._W_emb_left
+
+    @property
+    def W_emb_middle(self):
+        return self._W_emb_middle
+
+    @property
+    def W_emb_right(self):
+        return self._W_emb_right
+
+    def assign_lr(self, session, lr_value):
+        session.run(tf.assign(self.lr, lr_value))
+
+    def assign_embedding(self, session, pretrained):
+        session.run(tf.assign(self.W_emb_left, pretrained))
+        session.run(tf.assign(self.W_emb_middle, pretrained))
+        session.run(tf.assign(self.W_emb_right, pretrained))
---
+++ train_context.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+import time
+import os
+import tensorflow as tf
+import numpy as np
+
+import cnn_context
+import util
+
+
+FLAGS = tf.app.flags.FLAGS
+
+# train parameters
+this_dir = os.path.abspath(os.path.dirname(__file__))
+tf.app.flags.DEFINE_string('data_dir', os.path.join(this_dir, 'data', 'mlmi'), 'Directory of the data')
+tf.app.flags.DEFINE_string('train_dir', os.path.join(this_dir, 'train'),
+                           'Directory to save training checkpoint files')
+tf.app.flags.DEFINE_integer('num_epochs', 10, 'Number of epochs to run')
+tf.app.flags.DEFINE_boolean('use_pretrain', False, 'Use word2vec pretrained embeddings or not')
+tf.app.flags.DEFINE_boolean('log_device_placement', False, 'Whether log device information in summary')
+
+tf.app.flags.DEFINE_string('optimizer', 'adam',
+                           'Optimizer to use. Must be one of "sgd", "adagrad", "adadelta" and "adam"')
+tf.app.flags.DEFINE_float('init_lr', 0.01, 'Initial learning rate')
+tf.app.flags.DEFINE_float('lr_decay', 0.95, 'LR decay rate')
+tf.app.flags.DEFINE_integer('tolerance_step', 500,
+                            'Decay the lr after loss remains unchanged for this number of steps')
+tf.app.flags.DEFINE_float('dropout', 0.5, 'Dropout rate. 0 is no dropout.')
+
+
+# logging
+tf.app.flags.DEFINE_integer('log_step', 10, 'Display log to stdout after this step')
+tf.app.flags.DEFINE_integer('summary_step', 50,
+                            'Write summary (evaluate model on dev set) after this step')
+tf.app.flags.DEFINE_integer('checkpoint_step', 100, 'Save model after this step')
+
+
+def train(train_data, test_data):
+    # train_dir
+    timestamp = str(int(time.time()))
+    out_dir = os.path.abspath(os.path.join(FLAGS.train_dir, timestamp))
+
+    # save flags
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+    FLAGS._parse_flags()
+    config = dict(FLAGS.__flags.items())
+
+    # Window_size must not be larger than the sent_len
+    if config['sent_len'] < config['max_window']:
+        config['max_window'] = config['sent_len']
+
+    # flag to restore the contextwise model
+    config['contextwise'] = True
+
+    # save flags
+    util.dump_to_file(os.path.join(out_dir, 'flags.cPickle'), config)
+
+    # max number of steps
+    num_batches_per_epoch = int(np.ceil(float(len(train_data))/FLAGS.batch_size))
+    max_steps = num_batches_per_epoch * FLAGS.num_epochs
+
+
+
+    with tf.Graph().as_default():
+        with tf.variable_scope('cnn', reuse=None):
+            m = cnn_context.Model(config, is_train=True)
+        with tf.variable_scope('cnn', reuse=True):
+            mtest = cnn_context.Model(config, is_train=False)
+
+        # checkpoint
+        saver = tf.train.Saver(tf.all_variables())
+        save_path = os.path.join(out_dir, 'model.ckpt')
+        summary_op = tf.merge_all_summaries()
+
+        # session
+        sess = tf.Session(config=tf.ConfigProto(log_device_placement=FLAGS.log_device_placement))
+        with sess.as_default():
+            train_summary_writer = tf.train.SummaryWriter(os.path.join(out_dir, "train"), graph=sess.graph)
+            dev_summary_writer = tf.train.SummaryWriter(os.path.join(out_dir, "dev"), graph=sess.graph)
+            sess.run(tf.initialize_all_variables())
+
+            # assign pretrained embeddings
+            if FLAGS.use_pretrain:
+                print "Initialize model with pretrained embeddings..."
+                pretrained_embedding = np.load(os.path.join(FLAGS.data_dir, 'emb.npy'))
+                m.assign_embedding(sess, pretrained_embedding)
+
+            # initialize parameters
+            current_lr = FLAGS.init_lr
+            lowest_loss_value = float("inf")
+            decay_step_counter = 0
+            global_step = 0
+
+            # evaluate on dev set
+            def dev_step(mtest, sess):
+                dev_loss = []
+                dev_auc = []
+                dev_f1_score = []
+
+                # create batch
+                test_batches = util.batch_iter(test_data, batch_size=FLAGS.batch_size, num_epochs=1, shuffle=False)
+                for batch in test_batches:
+                    left_batch, middle_batch, right_batch, y_batch, _ = zip(*batch)
+                    #a_batch = np.ones((len(batch), 1), dtype=np.float32) / len(batch) # average
+                    feed = {mtest.left: np.array(left_batch),
+                            mtest.middle: np.array(middle_batch),
+                            mtest.right: np.array(right_batch),
+                            mtest.labels: np.array(y_batch)}
+                    #if FLAGS.attention:
+                    #    feed[mtest.attention] = np.array(a_batch)
+                    loss_value, eval_value = sess.run([mtest.total_loss, mtest.eval_op], feed_dict=feed)
+                    dev_loss.append(loss_value)
+                    pre, rec = zip(*eval_value)
+                    dev_auc.append(util.calc_auc_pr(pre, rec))
+                    dev_f1_score.append((2.0 * pre[5] * rec[5]) / (pre[5] + rec[5])) # threshold = 0.5
+
+                return (np.mean(dev_loss), np.mean(dev_auc), np.mean(dev_f1_score))
+
+            # train loop
+            print "\nStart training\n"
+            train_loss = []
+            train_auc = []
+            train_f1_score = []
+            train_batches = util.batch_iter(train_data, batch_size=FLAGS.batch_size, num_epochs=FLAGS.num_epochs)
+            for batch in train_batches:
+                batch_size = len(batch)
+
+                m.assign_lr(sess, current_lr)
+                global_step += 1
+
+                left_batch, middle_batch, right_batch, y_batch, a_batch = zip(*batch)
+                feed = {m.left: np.array(left_batch),
+                        m.middle: np.array(middle_batch),
+                        m.right: np.array(right_batch),
+                        m.labels: np.array(y_batch)}
+                if FLAGS.attention:
+                    feed[m.attention] = np.array(a_batch)
+                start_time = time.time()
+                _, loss_value, eval_value = sess.run([m.train_op, m.total_loss, m.eval_op], feed_dict=feed)
+                proc_duration = time.time() - start_time
+                train_loss.append(loss_value)
+                pre, rec = zip(*eval_value)
+                auc = util.calc_auc_pr(pre, rec)
+                f1 = (2.0 * pre[5] * rec[5]) / (pre[5] + rec[5]) # threshold = 0.5
+                train_auc.append(auc)
+                train_f1_score.append(f1)
+
+                assert not np.isnan(loss_value), "Model loss is NaN."
+
+                # print log
+                if global_step % FLAGS.log_step == 0:
+                    examples_per_sec = batch_size / proc_duration
+                    format_str = '%s: step %d/%d, f1 = %.4f, auc = %.4f, loss = %.4f ' + \
+                                 '(%.1f examples/sec; %.3f sec/batch), lr: %.6f'
+                    print format_str % (datetime.now(), global_step, max_steps, f1, auc, loss_value,
+                                        examples_per_sec, proc_duration, current_lr)
+
+
+
+
+                # write summary
+                if global_step % FLAGS.summary_step == 0:
+                    summary_str = sess.run(summary_op)
+                    train_summary_writer.add_summary(summary_str, global_step)
+                    dev_summary_writer.add_summary(summary_str, global_step)
+
+                    # summary loss, f1
+                    train_summary_writer.add_summary(
+                        _summary_for_scalar('loss', np.mean(train_loss)), global_step=global_step)
+                    train_summary_writer.add_summary(
+                        _summary_for_scalar('auc', np.mean(train_auc)), global_step=global_step)
+                    train_summary_writer.add_summary(
+                        _summary_for_scalar('f1', np.mean(train_f1_score)), global_step=global_step)
+
+                    dev_loss, dev_auc, dev_f1 = dev_step(mtest, sess)
+                    dev_summary_writer.add_summary(
+                        _summary_for_scalar('loss', dev_loss), global_step=global_step)
+                    dev_summary_writer.add_summary(
+                        _summary_for_scalar('auc', dev_auc), global_step=global_step)
+                    dev_summary_writer.add_summary(
+                        _summary_for_scalar('f1', dev_f1), global_step=global_step)
+
+                    print "\n===== write summary ====="
+                    print "%s: step %d/%d: train_loss = %.6f, train_auc = %.4f train_f1 = %.4f" \
+                          % (datetime.now(), global_step, max_steps,
+                             np.mean(train_loss), np.mean(train_auc), np.mean(train_f1_score))
+                    print "%s: step %d/%d:   dev_loss = %.6f,   dev_auc = %.4f   dev_f1 = %.4f\n" \
+                          % (datetime.now(), global_step, max_steps, dev_loss, dev_auc, dev_f1)
+
+                    # reset container
+                    train_loss = []
+                    train_auc = []
+                    train_f1_score = []
+
+
+
+                # decay learning rate if necessary
+                if loss_value < lowest_loss_value:
+                    lowest_loss_value = loss_value
+                    decay_step_counter = 0
+                else:
+                    decay_step_counter += 1
+                if decay_step_counter >= FLAGS.tolerance_step:
+                    current_lr *= FLAGS.lr_decay
+                    print '%s: step %d/%d, Learning rate decays to %.5f' % \
+                          (datetime.now(), global_step, max_steps, current_lr)
+                    decay_step_counter = 0
+
+                # stop learning if learning rate is too low
+                if current_lr < 1e-5:
+                    break
+
+
+
+                # save checkpoint
+                if global_step % FLAGS.checkpoint_step == 0:
+                    saver.save(sess, save_path, global_step=global_step)
+            saver.save(sess, save_path, global_step=global_step)
+
+
+def _summary_for_scalar(name, value):
+    return tf.Summary(value=[tf.Summary.Value(tag=name, simple_value=float(value))])
+
+
+def main(argv=None):
+    if not os.path.exists(FLAGS.train_dir):
+        os.mkdir(FLAGS.train_dir)
+
+    # load contextwise dataset
+    source_path = os.path.join(FLAGS.data_dir, 'ids')
+    target_path = os.path.join(FLAGS.data_dir, 'target.txt')
+    attention_path = None
+    if FLAGS.attention:
+        if os.path.exists(os.path.join(FLAGS.data_dir, 'source.att')):
+            attention_path = os.path.join(FLAGS.data_dir, 'source.att')
+        else:
+            raise ValueError("Attention file %s not found.", os.path.join(FLAGS.data_dir, 'source.att'))
+    train_data, test_data = util.read_data_contextwise(source_path, target_path, FLAGS.sent_len, attention_path)
+    train(train_data, test_data)
+
+
+if __name__ == '__main__':
+    tf.app.run()
